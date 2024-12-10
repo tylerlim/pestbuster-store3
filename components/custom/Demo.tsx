82bc9cb80eaecf5c363b8510296b91815246aed6
Tyler Lim <tylerjsc@gmail.com>
2024-12-11 03:47:05 +0800

    added

---
+++ components/custom/Demo.tsx
@@ -0,0 +1,12 @@
+import { Box, HStack } from "@chakra-ui/react"
+
+const Demo = () => {
+  return (
+    <HStack>
+      <Box h="10" bg="gray.100" rounded="lg" />
+      <Box h="10" bg="gray.100" rounded="lg" />
+    </HStack>
+  )
+}
+
+export default Demo
